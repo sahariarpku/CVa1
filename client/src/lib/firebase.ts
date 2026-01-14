983a8d3efb7dd3feeadf5ff0e9074387e0bca167
Sahariar Rahman <sahariarrahman@Sahariars-MacBook-Pro.local>
2026-01-14 22:40:25 +0000

    Refactor Firebase config to use environment variables

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getStorage } from "firebase/storage";
 
 // User provided config
 const firebaseConfig = {
-    apiKey: "key",
-    authDomain: "cv.firebaseapp.com",
-    projectId: "id",
-    storageBucket: "cv.link.app",
-    messagingSenderId: "oll",
-    appId: "1::web:",
-    measurementId: "G-"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
