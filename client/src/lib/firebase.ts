4622fba0e36dbbd8f37868e2eec2ddf7e810f74a
sahariarpku <90094059+sahariarpku@users.noreply.github.com>
2026-01-14 21:50:03 +0000

    Update firebase.ts

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getStorage } from "firebase/storage";
 
 // User provided config
 const firebaseConfig = {
-    apiKey: "AIzaSyAcuG1Js5TAu_ioPyx_MREd8tgb_WLyKwo",
-    authDomain: "cv-ai-7050e.firebaseapp.com",
-    projectId: "cv-ai-7050e",
-    storageBucket: "cv-ai-7050e.firebasestorage.app",
-    messagingSenderId: "531447719118",
-    appId: "1:531447719118:web:b127a075c0dca01dff1c9f",
-    measurementId: "G-7XBZG1S0SG"
+    apiKey: "key",
+    authDomain: "cv.firebaseapp.com",
+    projectId: "id",
+    storageBucket: "cv.link.app",
+    messagingSenderId: "oll",
+    appId: "1::web:",
+    measurementId: "G-"
 };
 
 // Initialize Firebase
